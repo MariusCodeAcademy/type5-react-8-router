ceac6047fb09dee6385e08e13790c5b02fc35862
Marius <marius@example.com>
2022-04-01 16:27:50 +0300

    pages

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,10 @@
+const Contact = () => {
+  return (
+    <div>
+      <h1>I am Contact page</h1>
+      <p>This is with router</p>
+    </div>
+  );
+};
+
+export default Contact;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>I am Home page</h1>
+      <p>This is with router</p>
+    </div>
+  );
+};
+
+export default Home;
