0ff497dc443fc4907f465428767c2788d4357c5a
Marius <marius@example.com>
2022-04-01 17:02:25 +0300

    Basic routetr works

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # react Router
+
+1. instaliuojam `npm install react-router-dom@^5`
+2. index.js apjuosi <App /> su BrowserRouter
--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { Route, Link } from 'react-router-dom';
 import './App.css';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className='App'>
-      <h1>App</h1>
+      <nav>
+        <Link className='nav-link' to='/home'>
+          Home page
+        </Link>
+        <Link to='/about'>About page</Link>
+        <Link to='/contact'>Contact page</Link>
+      </nav>
+      <Route path='/home'>
+        <Home />
+      </Route>
+      <Route path='/about'>
+        <About />
+      </Route>
+      <Route path='/contact'>
+        <Contact />
+      </Route>
     </div>
   );
 }
